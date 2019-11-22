887386e737c92ca8362748308ede9c39e565d343
Colin <cstatro@gmail.com>
2019-11-21 22:05:02 -0500

    remigrated

--- doctor_patients/doctor_patients/settings.py
+++ doctor_patients/doctor_patients/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'patients',
     'doctors',
     'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- doctor_patients/patients/views.py
+++ doctor_patients/patients/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Patient
 from .serializers import PatientSerializer
 
 
@@ -13,3 +14,8 @@ class PatientsListView(APIView):
             return Response(patient_record, status=status.HTTP_201_CREATED)
         else:
             return Response(patient.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        patients = Patient.objects.all()
+        serialized_patients = PatientSerializer(patients, many=True)
+        return Response(serialized_patients.data)
