d37b61bf2f8e0142bad1d17e88a08e4d0de5285e
Colin <cstatro@gmail.com>
2019-11-22 17:33:46 -0500

    added the join table fields to the model

--- doctor_patients/appointments/models.py
+++ doctor_patients/appointments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ..doctors.models import Doctor
+from ..patients.models import Patient
 
 # Create your models here.
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
