07391e9fe369b4c1522e0b330d8a5cd25109d1d9
Colin <cstatro@gmail.com>
2019-11-23 15:44:51 -0500

    added in the main list view for doctors

---
+++ doctor_patients/doctors/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from .views import DoctorListView
+
+urlpatterns = [path('doctors', DoctorListView.as_view())]
--- doctor_patients/doctors/views.py
+++ doctor_patients/doctors/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView, Response
+from .models import Doctor
+from .serializers import DoctorSerializer
 # Create your views here.
+
+
+class DoctorListView(APIView):
+    def get(self, request, format=None):
+        doctors = Doctor.objects.all()
+        serialized = DoctorSerializer(doctors, many=True)
+        return Response(serialized.data)
